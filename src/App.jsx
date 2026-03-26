720a5b65b2dd5ede316a061a17f8558335713c95
Valcin_Greg <valcingreg10@gmail.com>
2026-03-26 07:06:01 -0400

    EmployeForm upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import EmployeeForm from './components/EmployeeForm'
 
 import './App.css'
 
 function App() {
- 
+//  const [employees, setEmployees] = useState(initialEmployees);
+
+//   function handleAdd(newEmployee) {
+//     setEmployees([...employees, newEmployee]);
+//   }
 
   return (
     <>
+    <EmployeeForm  />
       
     </>
   )
